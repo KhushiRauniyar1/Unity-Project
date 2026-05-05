f188c5c17251e94b0d1f7176cc141c34c7a37685
KhushiRauniyar1 <khushirauniyar2024@gmail.com>
2026-05-05 20:29:16 +0545

    Created SwitchController script again and attached to SwitchButton

---
+++ Unity Project/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchController : MonoBehaviour
+{
+    public EnergyManager energyManager;
+    public Text switchLabel;
+
+    private bool isBulbOn = false;
+
+    void Start()
+    {
+        UpdateLabel();
+    }
+
+    // Connect this to Button's OnClick() event in Inspector
+    public void OnSwitchPressed()
+    {
+        isBulbOn = !isBulbOn;
+
+        energyManager.ToggleBulb();
+
+        UpdateLabel();
+    }
+
+    void UpdateLabel()
+    {
+        if (switchLabel != null)
+            switchLabel.text = isBulbOn ? "SWITCH OFF" : "SWITCH ON";
+    }
+}
